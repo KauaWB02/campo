7f4f42c150a2ef822e851cf3294dcc6ddc76c00d
kauawb02 <kauabatista254@gmail.com>
2024-06-22 02:51:34 -0300

    ajuste no mock

--- src/app/mock.ts
+++ src/app/mock.ts
@@ -3,7 +3,7 @@ export const moock = {
   name: 'Questão 1.0',
   customFieldType: 'CheckBox',
   description: 'Primeira Questão',
-  value: [], // Vai ja vir selecionar bote o id da alternativa abaixo exemplo:  [2]
+  value: [], // Para ja vir selecionar bote o id da alternativa abaixo exemplo:  [2]
   alternatives: [
     {
       id: 2,
@@ -20,7 +20,6 @@ export const moock = {
             {
               id: 4,
               name: 'opção 2.1',
-              isSelected: false,
               subQuestion: [
                 {
                   id: 5,
@@ -31,26 +30,172 @@ export const moock = {
                     {
                       id: 6,
                       name: 'opção 3.1',
-                      isSelected: false,
                       description: 'Só uma opção',
+                      subQuestion: [
+                        {
+                          id: 11,
+                          name: 'Sub Questão 4.0',
+                          customFieldType: 'CheckBox',
+                          value: [],
+                          alternatives: [
+                            {
+                              id: 12,
+                              name: 'opção 4.1',
+                              description: 'Só uma opção',
+                            },
+                            {
+                              id: 13,
+                              name: 'opção 4.2',
+                              description: 'Só uma opção',
+                            },
+                            {
+                              id: 14,
+                              name: 'opção 4.3',
+                              description: 'Só uma opção',
+                            },
+                          ],
+                        },
+                      ],
                     },
                     {
                       id: 7,
                       name: 'opção 3.2',
-                      isSelected: false,
                       description: 'Só uma opção',
                     },
                     {
                       id: 8,
                       name: 'opção 3.3',
-                      isSelected: false,
                       description: 'Só uma opção',
                     },
                     {
                       id: 9,
                       name: 'opção 3.4',
-                      isSelected: false,
                       description: 'Só uma opção',
+                      subQuestion: [
+                        {
+                          id: 15,
+                          name: 'Sub Questão 5.0',
+                          customFieldType: 'CheckBox',
+                          value: [],
+                          alternatives: [
+                            {
+                              id: 16,
+                              name: 'opção 5.1',
+                              description: 'Só uma opção',
+                              subQuestion: [
+                                {
+                                  id: 19,
+                                  name: 'Sub Questão 6.0',
+                                  customFieldType: 'CheckBox',
+                                  value: [],
+                                  alternatives: [
+                                    {
+                                      id: 20,
+                                      name: 'opção 6.1',
+                                      description: 'Só uma opção',
+                                    },
+                                    {
+                                      id: 21,
+                                      name: 'opção 6.2',
+                                      description: 'Só uma opção',
+                                    },
+                                    {
+                                      id: 23,
+                                      name: 'opção 6.3',
+                                      description: 'Só uma opção',
+                                      subQuestion: [
+                                        {
+                                          id: 24,
+                                          name: 'Sub Questão 7.0',
+                                          customFieldType: 'CheckBox',
+                                          value: [],
+                                          alternatives: [
+                                            {
+                                              id: 25,
+                                              name: 'opção 7.1',
+                                              description: 'Só uma opção',
+                                            },
+                                            {
+                                              id: 26,
+                                              name: 'opção 7.2',
+                                              description: 'Só uma opção',
+                                            },
+                                            {
+                                              id: 27,
+                                              name: 'opção 7.3',
+                                              description: 'Só uma opção',
+                                              subQuestion: [
+                                                {
+                                                  id: 28,
+                                                  name: 'Sub Questão 8.0',
+                                                  customFieldType: 'CheckBox',
+                                                  value: [],
+                                                  alternatives: [
+                                                    {
+                                                      id: 29,
+                                                      name: 'opção 8.1',
+                                                      description: 'Só uma opção',
+                                                    },
+                                                    {
+                                                      id: 30,
+                                                      name: 'opção 8.2',
+                                                      description: 'Só uma opção',
+                                                      subQuestion: [
+                                                        {
+                                                          id: 31,
+                                                          name: 'Sub Questão 9.0',
+                                                          customFieldType: 'CheckBox',
+                                                          value: [],
+                                                          alternatives: [
+                                                            {
+                                                              id: 32,
+                                                              name: 'opção 9.1',
+                                                              description: 'Só uma opção',
+                                                            },
+                                                            {
+                                                              id: 33,
+                                                              name: 'opção 9.2',
+                                                              description: 'Só uma opção',
+                                                            },
+                                                            {
+                                                              id: 34,
+                                                              name: 'opção 9.3',
+                                                              description: 'Só uma opção',
+                                                            },
+                                                          ],
+                                                        },
+                                                      ],
+                                                    },
+                                                    {
+                                                      id: 31,
+                                                      name: 'opção 8.3',
+                                                      description: 'Só uma opção',
+                                                    },
+                                                  ],
+                                                },
+                                              ],
+                                            },
+                                          ],
+                                        },
+                                      ],
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              id: 17,
+                              name: 'opção 5.2',
+                              description: 'Só uma opção',
+                            },
+                            {
+                              id: 18,
+                              name: 'opção 5.3',
+                              description: 'Só uma opção',
+                            },
+                          ],
+                        },
+                      ],
                     },
                   ],
                 },
